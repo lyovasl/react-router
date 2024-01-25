73e06f03e4298b6f8af9e71192279604e4342518
Levon Aslanyan <aslanyan2012@mail.ru>
2024-01-26 00:31:36 +0400

    Route Outlet

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const BookList = () => {
   return (
     <>
-      <div>Book List</div>
+      <h1>Book List </h1>
       <div
         style={{
           display: "flex",
@@ -11,26 +11,7 @@ const BookList = () => {
           background: "green",
           color: "orange",
         }}
-      >
-        <Link
-          style={{ textDecoration: "none", color: "inherit" }}
-          to="/books/1"
-        >
-          Book 1
-        </Link>
-        <Link
-          style={{ textDecoration: "none", color: "inherit" }}
-          to="/books/2"
-        >
-          Book 2
-        </Link>
-        <Link
-          style={{ textDecoration: "none", color: "inherit" }}
-          to="/books/new"
-        >
-          New Book
-        </Link>
-      </div>
+      ></div>
     </>
   );
 };
